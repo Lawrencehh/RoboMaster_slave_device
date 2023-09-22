c09e1f7f88f8dd0889c049cee88c0e548b61b9a0
Lawrencehh <sy1607511@buaa.edu.cn>
2023-09-22 19:11:51 +0800

    1

--- HARDWARE/uart3.h
+++ HARDWARE/uart3.h
@@ -6,6 +6,8 @@
 void TX2_USART3_Init(void);
 void USART3_IRQHandler(void);
 void TX2_Transmit_Start(void);
+uint16_t calc_crc16_modbus(uint8_t *data, uint16_t length);
+
 float SXT_pitch(float target_pitch_position,float middle_pitch_position);
 float SXT_yaw(float target_yaw_position,float middle_yaw_position);
 void tmr2_callback(void *p_tmr, void *p_arg);
@@ -13,4 +15,6 @@ void tmr2_callback(void *p_tmr, void *p_arg);
 extern float Camera_Inc_Pitch,Camera_Inc_Yaw,Camera_Distance;
 extern u8 NewData_flag;
 extern int32_t snake_motor_position[12];
+extern const uint16_t crc16_table[256];
+
 #endif
--- TASK/chasis_task.c
+++ TASK/chasis_task.c
@@ -79,6 +79,50 @@ void Chasis_task(void *p_arg)
 				readEncorder(can_id,0x02,0x03,0x07);
 			}
 			
+			  uint8_t packet[68] = {0}; 
+				
+				// header: 0xAA55
+				packet[0] = 0xAA;
+				packet[1] = 0x55;
+
+				// id: 0x01
+				packet[2] = 0x01;
+
+				// data length: 0x40 (1 + 1 + 12 * 5 + 2)
+				packet[3] = 0x40;
+
+				// function code: 0x41
+				packet[4] = 0x41;
+
+				// motors number: 0x0C (12)
+				packet[5] = 0x0C;
+
+				// sensors data
+				int idx = 6;
+				for (int i = 0; i < 12; ++i) {
+						packet[idx++] = i + 1;  // encorder addresss
+						// encorder data
+						packet[idx++] = (currentPosition[i] >> 24) & 0xFF;  
+						packet[idx++] = (currentPosition[i] >> 16) & 0xFF;
+						packet[idx++] = (currentPosition[i] >> 8) & 0xFF;
+						packet[idx++] = (currentPosition[i] 		) & 0xFF;
+						
+				}
+
+				// CRC-16 modbus
+				uint16_t crc = calc_crc16_modbus(packet, idx);
+				packet[idx++] = (crc >> 8) & 0xFF; 
+				packet[idx++] = crc & 0xFF;
+
+				
+				
+
+			for(uint8_t i=0;i< sizeof(packet);i++){
+				// send to the usart3
+				USART_ClearFlag(USART3,USART_FLAG_TC);
+				USART_SendData(USART3,packet[i]);
+				while(USART_GetFlagStatus(USART3,USART_FLAG_TXE)==RESET);//判断是否发送完成
+			}
 				
 			
 				
